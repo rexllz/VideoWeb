fed3560f1299ed094007bcc847a0fe8986b0ac15
rexllz <lzliurex@gmail.com>
2019-09-01 21:38:10 +0800

    stream set up

---
+++ streamserver/handlers.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Param)  {
+
+
+}
+
+func uploadHandler()  {
+	
+}
