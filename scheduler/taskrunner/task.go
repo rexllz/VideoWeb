aab058ad15c106d66b131a23d1b0f35ed79b3aed
rexllz <lzliurex@gmail.com>
2019-09-10 00:14:41 +0800

    finish Video Deletion dbops/task

--- scheduler/taskrunner/task.go
+++ scheduler/taskrunner/task.go
@@ -1,1 +1,74 @@
 package taskrunner
+
+import (
+	"VideoWeb/scheduler/dbops"
+	"errors"
+	"log"
+	"os"
+	"sync"
+)
+
+func deleteVideo(vid string) error {
+
+	err := os.Remove(VIDEO_PATH + vid)
+
+	if err != nil && !os.IsNotExist(err){
+		log.Printf("Deleting Video File Error : %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func VideoClearDispatcher(dc dataChan) error {
+
+	res, err := dbops.ReadVideoDeletionRecord(3)
+	if err != nil{
+		log.Printf("Video Clear Dispatcher Error : %v", err)
+		return err
+	}
+
+	if len(res) == 0 {
+		return errors.New("all task finished")
+	}
+
+	for _,id := range res {
+		dc <- id
+	}
+
+	return nil
+}
+
+func VideoClearExecutor(dc dataChan) error {
+
+	errMap := &sync.Map{}
+	var err error
+
+	forloop:
+		for {
+			select {
+			case vid :=<- dc :
+				go func(id interface{}) {
+					if err := deleteVideo(id.(string)); err != nil{
+						errMap.Store(id, err)
+						return
+					}
+					if err := dbops.DelVideoDeletionRecord(id.(string)); err != nil{
+						errMap.Store(id, err)
+						return
+					}
+				}(vid)
+			default:
+				break forloop
+			}
+		}
+	errMap.Range(func(key, value interface{}) bool {
+		err = value.(error)
+		if err !=nil {
+			return false
+		}
+		return true
+	})
+
+	return err
+}
