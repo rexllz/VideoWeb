a8ffb841842e3770019aea97c8b6a76d5905ff8e
rexllz <lzliurex@gmail.com>
2019-09-17 23:44:00 +0800

    finish the main.go serve frame

--- web/main.go
+++ web/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandler() *httprouter.Router {
+
+	router := httprouter.New()
+	router.GET("/",homeHandler)
+	router.POST("/",homeHandler)
+	router.GET("/userhome",userHomeHandler)
+	router.POST("/userhome",userHomeHandler)
+	router.POST("/api",apiHandler)
+	router.ServeFiles("/statics/*filepath",http.Dir("web/template"))
+
+	return router
+}
+
+func main()  {
+
+	r := RegisterHandler()
+	http.ListenAndServe(":8080",r)
+}
